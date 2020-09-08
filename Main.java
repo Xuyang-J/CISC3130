b5c536a1cf7750bb8fe8b3e9f514cad07a3d5f79
Xuyang-J <48388615+Xuyang-J@users.noreply.github.com>
2020-09-08 11:26:36 -0400

    Submission for Lab#2
    Added additional comments

--- Main.java
+++ Main.java
@@ -22,7 +22,7 @@ public class Main {
             }
             i++;
         }
-        output.printf("%-13s%-38s%-40s%-32s%n","Ranking" ,"Song Name" ,"Artists" ,"Streams"); //code for outputting and formatting to a txt file
+        output.printf("%-13s%-38s%-40s%-32s%n","Ranking" ,"Song Name" ,"Artists" ,"Streams"); //code for outputting and formatting to a txt file.
         for(int j = 0; j < 189; j++) {
             output.printf("%-13s%-42s%-35s%-35s%n",list[j][0],list[j][1],list[j][2],list[j][3]);
 

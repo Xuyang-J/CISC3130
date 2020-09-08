8555c79719e89daeda0f2b0ad177e7efef84564e
Xuyang-J <48388615+Xuyang-J@users.noreply.github.com>
2020-09-08 11:26:04 -0400

    Main.java
    added additional comments

--- Main.java
+++ Main.java
@@ -2,19 +2,19 @@ import java.util.*;
 import java.io.*;
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException { // main method used to execute the main code.
         splitInput();
 
     }
-    public static void splitInput() throws FileNotFoundException {
-        PrintStream output = new PrintStream("output.txt");
-        String[][] list = new String[189][7];
-        File file = new File("streams.csv");
-        Scanner sc = new Scanner(file);
-        String temp = "";
-        String[] tempArr;
+    public static void splitInput() throws FileNotFoundException { // main code 
+        PrintStream output = new PrintStream("output.txt");// PrintStream setup for outputting to a file.
+        String[][] list = new String[189][7]; // initializing the nested array that will be used to store the data
+        File file = new File("streams.csv");// file setup for scanner to read in
+        Scanner sc = new Scanner(file); // scanner setup
+        String temp = ""; // will be used to store lines of string from input
+        String[] tempArr; // will be used to store individual lines of data from split of temp.
         int i = 0;
-        while(sc.hasNextLine()) {
+        while(sc.hasNextLine()) { //while + for loop to assign data read from file into nested array
             temp = sc.nextLine();
             tempArr = temp.split(",");
             for(int j = 0; j < tempArr.length; j++) {
@@ -22,7 +22,7 @@ public class Main {
             }
             i++;
         }
-        output.printf("%-13s%-38s%-40s%-32s%n","Ranking" ,"Song Name" ,"Artists" ,"Streams");
+        output.printf("%-13s%-38s%-40s%-32s%n","Ranking" ,"Song Name" ,"Artists" ,"Streams"); //code for outputting and formatting to a txt file
         for(int j = 0; j < 189; j++) {
             output.printf("%-13s%-42s%-35s%-35s%n",list[j][0],list[j][1],list[j][2],list[j][3]);
 

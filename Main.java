8eaac70b7cfa7c5ecf5e6dcb7117d8ff8305668e
Xuyang-J <48388615+Xuyang-J@users.noreply.github.com>
2020-10-05 21:50:03 -0400

    Lab #3
    Source code, input, and output file

--- Main.java
+++ Main.java
@@ -2,31 +2,50 @@ import java.util.*;
 import java.io.*;
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException { // main method used to execute the main code.
+    public static void main(String[] args) throws FileNotFoundException { // main method used to execute other methods
         splitInput();
 
     }
-    public static void splitInput() throws FileNotFoundException { // main code 
-        PrintStream output = new PrintStream("Artists-WeekOf09082020.txt");// PrintStream setup for outputting to a file.
-        String[][] list = new String[189][7]; // initializing the nested array that will be used to store the data
-        File file = new File("streams.csv");// file setup for scanner to read in
-        Scanner sc = new Scanner(file); // scanner setup
-        String temp = ""; // will be used to store lines of string from input
-        String[] tempArr; // will be used to store individual lines of data from split of temp.
+
+    public static void splitInput() throws FileNotFoundException { // takes input from csv file and split it into 2D array
+                                                                        // which is then printed out in a formatted manner
+        PrintStream output = new PrintStream("Artists-WeekOf10-01-2020.txt"); // PrintStream used to output to a file
+        String[][] list = new String[199][6]; // 2D array used to store split inputs
+        File file = new File("input.csv"); // path to input file
+        Scanner sc = new Scanner(file);
+        String temp;
+        String[] tempArr;
         int i = 0;
-        while(sc.hasNextLine()) { //while + for loop to assign data read from file into nested array
+        while (sc.hasNextLine()) { // while loop to read in data from input and store it in 2D array
             temp = sc.nextLine();
             tempArr = temp.split(",");
-            for(int j = 0; j < tempArr.length; j++) {
-                list[i][j] = tempArr[j];
-            }
+            System.arraycopy(tempArr, 0, list[i], 0, tempArr.length);
             i++;
         }
-        output.printf("%-13s%-38s%-40s%-32s%-32s%n","Ranking" ,"Song Name" ,"Artists" ,"Streams","URL"); //code for outputting and formatting to a txt file.
-        for(int j = 0; j < 189; j++) {
-            output.printf("%-13s%-42s%-35s%-35s%-40s%n",list[j][0],list[j][1],list[j][2],list[j][3],list[j][4]);
-
+        Iterator it = alphabeticalSort(list).iterator(); //iterator object that iterates through the Linked List to output
+        int num = 0;
+        while (it.hasNext()) {
+            output.printf("%-10s%s%n", num + 1, it.next());
+            num++;
         }
+    }
 
+    public static LinkedList alphabeticalSort(String[][] a) { // Sorting the artist name from A-Z and feeding it to a linked list
+        String[] arr = new String[199];
+        for (int i = 0; i < a.length; i++) {
+            arr[i] = a[i][2].replace('"', ' '); // Replaces the quotation marks that is present in some artist names
+            arr[i] = arr[i].trim(); // trims the spaces that replaces the quotation marks
+        }
+        String temp;
+        for (int j = 0; j < arr.length - 1; j++) {
+            for (int x = 0; x < arr.length - 1; x++) {
+                if (arr[x].substring(0, 1).compareToIgnoreCase(arr[x + 1].substring(0, 1)) > 0) { //sorts the names of the artist from A-Z
+                    temp = arr[x];                                                      // through bubble sort
+                    arr[x] = arr[x + 1];
+                    arr[x + 1] = temp;
+                }
+            }
+        }
+        return new LinkedList(Arrays.asList(arr));
     }
 }
